55b0dc664f184a9583b98440e08256609253e4f1
rishabharoraa <rishabharora.ns@gmail.com>
2024-07-29 22:26:40 +0530

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,15 @@
+var interval = setInterval(() => {
+    console.log("ping")
+    const codeEditors = document.getElementsByClassName("monaco-mouse-cursor-text");
+
+    for(let codeEditor of codeEditors) {  
+        const computedFontFamily = window.getComputedStyle(codeEditor).fontFamily;
+        if(computedFontFamily.includes("Jetbrains Mono")) {
+            clearInterval(interval);
+            return;
+        }
+
+        codeEditor.style.setProperty('font-family', 'Jetbrains Mono', 'important');
+        codeEditor.style.setProperty('font-size', '14', 'important');
+    }
+}, 500);
